84ed046230174e8d7be77ad4ddaeedf1b20b78a4
Suzanna <suzantelecan.exceedteam@gmail.com>
2019-03-25 12:13:57 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,39 @@
+$(document).ready(function(){
+	$("#msgid").html("");
+ });
+ 
+ arr.shift();
+
+
+
+
+ // Global declaration of list 
+function removeItem() {
+  $(this).closest('li').remove()
+  toggleEmptyState();
+}
+
+function toggleEmptyState() {
+  debugger
+  if ($("li").length > 0) {
+    $('.help').css('display', 'none');
+  } else {
+    $('.help').css('display', 'block');
+  }
+}
+
+function newItem() {
+  var newIter = $("li").length + 1;
+  var inputValue = $("#todo-input").val();
+  if (inputValue === '') {
+    alert("For Gods Sake Just type something before adding anything!");
+  } else {
+    var li = "<li data-delete-id='" + newIter + "'>" + inputValue + "</li>";
+    $('#myUL').append(li);
+    $("#todo-input").val("");
+    var span = "<span class='close'>\u00D7</span>";
+    $("[data-delete-id='" + newIter + "']").append(span);
+    $('.close').off('click').on('click', removeItem);
+  }
+  toggleEmptyState()
+}
